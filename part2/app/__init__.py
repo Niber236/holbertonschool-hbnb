ab315acc82a18b408c70d5293e17d94600029e9e
Niber236 <tonmail@gmail.com>
2026-02-27 15:41:27 +0100

    Task 2: Implement User API endpoints

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_restx import Api
+from app.api.v1.users import api as users_ns
 
 def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
-    # Placeholder pour les routes de l'API (on les ajoutera plus tard)
-
+    # Enregistrement du namespace Users
+    api.add_namespace(users_ns, path='/api/v1/users')
     return app
--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,4 +1,5 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
 
 class HBnBFacade:
     def __init__(self):
@@ -7,10 +8,30 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder pour créer un utilisateur
+    # --- LOGIQUE UTILISATEUR (USER) ---
+    
     def create_user(self, user_data):
-        pass
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
 
-    # Placeholder pour récupérer un lieu
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+
+    def get_all_users(self):
+        return self.user_repo.get_all()
+
+    def update_user(self, user_id, user_data):
+        user = self.user_repo.get(user_id)
+        if not user:
+            return None
+        user.update(user_data)
+        return user
+
+    # --- LOGIQUE LIEU (PLACE) - À faire plus tard ---
+    
     def get_place(self, place_id):
         pass
