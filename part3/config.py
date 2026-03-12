e0dbc53f4e594c7bcb2ec604444f1918b4f97494
Niber236 <tonmail@gmail.com>
2026-03-12 17:48:40 +0100

    Correction du systeme de protection des mots de passe

--- part3/config.py
+++ part3/config.py
@@ -1,8 +1,8 @@
 import os
 
-class config:
-    SECRET_KEY = os.getnev('SECRET_KEY' , 'default_secret_key')
-    JWT_SECRET_KEY = 'stanisma_va_valider_son_rncp_12345!'
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY' , 'default_secret_key')
+    JWT_SECRET_KEY = 'stanima_va_valider_son_rncp_12345!'
     DEBUG = False
 
 class DevelopmentConfig(Config):
